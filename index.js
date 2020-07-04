9295a4dd4e4c9a5606be961732e103a9c5fe2bcd
unknown <fellypsantos2011@gmail.com>
2020-07-04 12:59:20 -0300

    Download automatico de PDF.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const app = express().use(cors());
+const routes = require('./src/routes');
+
+app.use(routes);
 
 app.get('/', (request, response) => {
-  response.send('Hello World');
+  response.send('Ambiente para geração de PDF do Cartão do SUS.');
 });
 
 app.listen(process.env.PORT || 3000);
